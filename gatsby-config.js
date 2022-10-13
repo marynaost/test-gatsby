791d136f140490e2b18720cdfbe283f2cc952c78
Maryna Ostapliuk <marynaum01@gmail.com>
2022-10-13 20:47:30 +0300

    added plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,16 @@
+const path = require("path");
+// Get paths of Gatsby's required rules, which as of writing is located at:
+// https://github.com/gatsbyjs/gatsby/tree/fbfe3f63dec23d279a27b54b4057dd611dce74bb/packages/
+// gatsby/src/utils/eslint-rules
+const gatsbyRequiredRules = path.join(
+  process.cwd(),
+  "node_modules",
+  "gatsby",
+  "dist",
+  "utils",
+  "eslint-rules"
+);
+
 module.exports = {
   siteMetadata: {
     title: "My Gatsby Site",
@@ -5,11 +18,41 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
         path: `${__dirname}/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/locales`,
+        name: `locale`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        rulePaths: [gatsbyRequiredRules],
+        stages: ["develop"],
+        extensions: ["js", "jsx", "ts", "tsx"],
+        exclude: ["node_modules", "bower_components", ".cache", "public"],
       },
     },
   ],
---
+++ src/cms/cms.js
@@ -0,0 +1,5 @@
+import CMS from "netlify-cms-app";
+
+import uploadcare from "netlify-cms-media-library-uploadcare";
+
+CMS.registerMediaLibrary(uploadcare);
